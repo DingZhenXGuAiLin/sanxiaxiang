fe867d5d3ef437c06476a8ff8060ab4066d52d4d
zhangtianyu <2455197302@qq.com>
2024-07-13 17:21:58 +0800

    first-version

---
+++ end_0/src/main/java/com/example/end_0/End0Application.java
@@ -0,0 +1,13 @@
+package com.example.end_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class End0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(End0Application.class, args);
+    }
+
+}
