7afb80cc2a12efb88421d66bd09f25b99eba192d
zhangtianyu <2455197302@qq.com>
2024-07-13 17:32:05 +0800

    111

--- end_0/src/main/java/com/example/end_0/End0Application.java
+++ end_0/src/main/java/com/example/end_0/End0Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class End0Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(End0Application.class, args);
     }
 
