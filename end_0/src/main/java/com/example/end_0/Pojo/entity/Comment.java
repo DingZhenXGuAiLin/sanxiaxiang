bdb9584919775a42a4a628ed6bb3b1fecd63633b
zhangtianyu <2455197302@qq.com>
2025-02-14 22:37:24 +0800

    新增comment对应每层内容

---
+++ end_0/src/main/java/com/example/end_0/Pojo/entity/Comment.java
@@ -0,0 +1,11 @@
+package com.example.end_0.Pojo.entity;
+
+import lombok.Data;
+
+@Data
+public class Comment {
+    Integer landscape_id;
+    Integer tourist_id;
+    String content;
+    String time;
+}
